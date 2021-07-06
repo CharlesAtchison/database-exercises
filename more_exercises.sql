8ffd7289bd1ccea4425396dfe2dd1bd155575174
CharlesAtchison <Charles.Atchison@outlook.com>
2021-07-06 16:33:55 -0500

    Modified more_exercises.sql and updated the file so that it is more accurate for question 1.

--- more_exercises.sql
+++ more_exercises.sql
@@ -1,6 +1,8 @@
 
 use germain_1455;
 
+
+
 /*
 How much do the current managers of each department get paid, relative to the average salary for the department? Is there any department where the department manager gets paid less than the average salary?
 */
